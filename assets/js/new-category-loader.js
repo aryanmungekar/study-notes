7cf8f1cbbee8c5c346b1e1d04fc15faaaf08d9af
Aryan Mungekar <aryan.mungekar05@gmail.com>
2025-07-21 15:27:23 +0530

    category loader

--- assets/js/new-category-loader.js
+++ assets/js/new-category-loader.js
@@ -10,7 +10,7 @@ const categories = {
 const headerBar = document.getElementById("headerBar");
 const initialButtonsDiv = document.getElementById("initialButtons"); // Optional (can be hidden)
 
-const subjectCode = window.location.pathname.match(/(\d{4})\.md$/)?.[1]; // e.g., '1000'
+const subjectCode = window.location.pathname.match(/(\d{4})\/?$/)?.[1]; // e.g., '1000'
 
 if (subjectCode) {
   fetch('./new-pdf-data.json')
