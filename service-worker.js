d280843d0f87dfc6d20f4539f0d580473443a7c0
Aryan Mungekar <aryan.mungekar05@gmail.com>
2025-08-02 13:07:08 +0530

    service worker version 4

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sppu-notes-v4.0";  // ⬅️ Increment this on every update!
+const CACHE_NAME = "sppu-notes-v4.1";  // ⬅️ Increment this on every update!
 
 const OFFLINE_FILES = [
   "/",
